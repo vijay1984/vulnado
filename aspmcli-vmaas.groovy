08fb03a6b5f001b908f163174cb1b8d8cf6f3450
mnemonic <chandranvijay.r@gmail.com>
2023-06-20 20:55:55 +0530

    Update aspmcli-vmaas.groovy

--- aspmcli-vmaas.groovy
+++ aspmcli-vmaas.groovy
@@ -3,8 +3,8 @@
 def ecrUrl = '${env.ECR_URL}'
 def runAspmScan() {
 	echo "Inside Run ASPM Scan" 
-	echo "ECR URL::: ${ecrUrl}"
-withDockerRegistry(credentialsId: 'ecr:us-west-2:0e60ebb5-69fa-47b0-8e1d-0dc79c3ed1df', url: '${ecrUrl}'){
+	withEnv(["AWS_ECR_URL=${ECR_URL}"]) {
+withDockerRegistry(credentialsId: 'ecr:us-west-2:0e60ebb5-69fa-47b0-8e1d-0dc79c3ed1df', url: '$AWS_ECR_URL'){
     sh script: "set +x; docker pull -q securin-aspm-cli:latest & > /dev/null"
     echo "ASPM Image pulled"
     sh script: "set +x; docker container create --name temp -v '${env.WORKSPACE}':/workdir ${aspmCliImageWithTag}"
@@ -17,6 +17,7 @@ withDockerRegistry(credentialsId: 'ecr:us-west-2:0e60ebb5-69fa-47b0-8e1d-0dc79c3
     echo "Scan Status ::: ${scanStatus}"
     return scanStatus
   }
+}
 }
 
 // TO RUN ASPM DOCKER CONTAINER SCAN
