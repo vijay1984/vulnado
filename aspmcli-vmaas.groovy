3ef7dea655858cf55c453a755d0a761d7ae779f0
mnemonic <chandranvijay.r@gmail.com>
2023-06-20 19:02:10 +0530

    Update aspmcli-vmaas.groovy - $ removed

--- aspmcli-vmaas.groovy
+++ aspmcli-vmaas.groovy
@@ -1,18 +1,17 @@
 // Below Can Be Passed as Parameters From Jenkins Pipeline Script if required:
-String aspmCliImageWithTag = "securin-aspm-cli:latest"
-
 // TO RUN ASPM SCAN
 def runAspmScan() {
-  withDockerRegistry(credentialsId: 'ASPM-CLI', url: ${env.ECR_URL}){
-    sh script: "set +x; docker pull -q ${aspmCliImageWithTag} & > /dev/null"
+	echo "Inside Run ASPM Scan"  
+withDockerRegistry(credentialsId: 'ASPM-CLI', url: ${env.ECR_URL}){
+    sh script: "set +x; docker pull -q securin-aspm-cli:latest & > /dev/null"
     echo "ASPM Image pulled"
-    sh script: "set +x; docker container create --name temp -v ${env.WORKSPACE}:/workdir ${aspmCliImageWithTag}"
+    sh script: "set +x; docker container create --name temp -v '${env.WORKSPACE}':/workdir ${aspmCliImageWithTag}"
     sh script: "set +x; docker cp '${env.WORKSPACE}/.' temp:/workdir"
     echo "Starting ASPM Scan"
     scanResponse = sh script: "set +x; docker run --name cli --volumes-from temp -v /var/run/docker.sock:/var/run/docker.sock -t ${aspmCliImageWithTag} -is_compiled=true -standalone", returnStdout: true
     echo "Scan Response:::: ${scanResponse}"
-    sh script: "set +x; docker cp cli:/workdir/results/status.txt ${env.WORKSPACE}/."
-    def scanStatus = readFile "${env.WORKSPACE}/status.txt"
+    sh script: "set +x; docker cp cli:/workdir/results/status.txt '${env.WORKSPACE}/.'"
+    def scanStatus = readFile "'${env.WORKSPACE}'/status.txt"
     echo "Scan Status ::: ${scanStatus}"
     return scanStatus
   }
