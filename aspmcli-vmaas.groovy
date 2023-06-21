3423d0e0f5d20bc39caf79bbe5dd6c462a699baf
mnemonic <chandranvijay.r@gmail.com>
2023-06-21 10:57:11 +0530

    Update aspmcli-vmaas.groovy

--- aspmcli-vmaas.groovy
+++ aspmcli-vmaas.groovy
@@ -8,10 +8,8 @@ def runAspmScan() {
 	def imageUrl = removePrefix("${AWS_ECR_URL}")
     sh "docker pull ${imageUrl}/securin-aspm-cli:latest"
     echo "ASPM Image pulled"
-    sh "docker container create --name temp -v '${env.WORKSPACE}':/workdir securin-aspm-cli:latest"
-    sh "docker cp '${env.WORKSPACE}/.' temp:/workdir"
     echo "Starting ASPM Scan"
-    def scanResponse = sh script: "set +x; docker run --name cli --volumes-from temp -v /var/run/docker.sock:/var/run/docker.sock -t securin-aspm-cli:latest -is_compiled=true -standalone", returnStdout: true
+    def scanResponse = sh script: "docker run --name cli -v ${env.WORKSPACE}:/workdir -v /var/run/docker.sock:/var/run/docker.sock -t ${imageUrl} -is_compiled=true -standalone", returnStdout: true
     echo "Scan Response:::: ${scanResponse}"
     sh script: "set +x; docker cp cli:/workdir/results/status.txt '${env.WORKSPACE}/.'"
     def scanStatus = readFile "'${env.WORKSPACE}'/status.txt"
